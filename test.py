0a1df0fd6cd56e94e7e788c6bcfdc9b8f3ad8189
Maggie <blackboxkun@gmail.com>
2024-06-26 08:44:12 +0800

    finish source 2 and part of source 3.

--- test.py
+++ test.py
@@ -78,39 +78,81 @@ copy_source1(23,"K")
 #copy 广告销量 col=24
 copy_source1(24,"L")
 
+#close source sheet1
+# source1.save("sample1.xlsx")
 
 #clean up source 2 sheet
 source2=load_workbook('sample2.xlsx')
 source_sheet2 = source2.active
 source_sheet2.delete_rows(1,amount=1)
 
-#make a funtion to find index of asin
-def find_index_source(lookup_value, lookup_col_num):
-    source_list=[]
-    for i,row in enumerate(source_sheet2):
+# #make a function to find index of asin
+# def find_index(source_sheet,lookup_value, lookup_col_num):
+#     source_list=[]
+#     for i,row in enumerate(source_sheet):
+#         if i==0:
+#             continue
+#         item=row[lookup_col_num].value
+#         source_list.append(item)
+#     target_index=source_list.index(lookup_value)
+#     return target_index
+#
+# #make a function to return the value of the look up value
+# def return_value(target_index,return_row_num):
+#     return_list=[]
+#     for i,row in enumerate(source_sheet2):
+#         if i==0:
+#             continue
+#         item=row[return_row_num].value
+#         return_list.append(item)
+#         return(return_list[target_index])
+
+#make a function the generate a dictionary of key_asin and
+# value_target value.
+def make_dict_source(source_sheet,lookup_col_num, return_col_num):
+    dict={}
+    for i,row in enumerate(source_sheet):
         if i==0:
             continue
-        item=row[lookup_col_num].value
-        source_list.append(item)
-    target_index=source_list.index(lookup_value)
-    return target_index
+        key=row[lookup_col_num].value
+        value=row[return_col_num].value
+        dict[key]=value
+    return dict
 
-#make a function to return the value of the look up value
-def return_valve(target_index,return_row_num,dest_col):
-    return_list=[]
-    for i,row in enumerate(source_sheet2):
-        if i==0:
-            continue
-        item=row[return_row_num].value
-        return_list.append(item)
-        return(return_list[target_index])
+# print(make_dict_source2(1,3))
+
+#fill the sheet_total with the dict from source2
+def fill_sheet_total(source_sheet,source_dict_col,source_return_col,
+                     lookup_col_num,
+                     target_col_num):
+
+    source_dict=make_dict_source(source_sheet,source_dict_col,
+                                 source_return_col)
+    for row in sheet_total:
+        row[target_col_num].value=source_dict.get(row[lookup_col_num].value,0)
+
+#fill 毛利润
+fill_sheet_total(source_sheet2,1,3,0,11)
+
+#fill 毛利率
+fill_sheet_total(source_sheet2,1,4,0,12)
+
+#close source sheet 2
+# source2.save("sample2.xlsx")
 
 
 
+#load source sheet 3
+source3=load_workbook('sample3.xlsx')
+source_sheet3 = source3.active
+
+#get the source dict from source 3
+make_dict_source(source_sheet3,1,2)
+
+def edit_source_dict(source_dict):
+    for key,value in source_dict.items():
+        source_dict[key]=value.split()
+    return source_dict
 
 
-
-
-
-
-
+print(make_dict_source(source_sheet3,1,2))
